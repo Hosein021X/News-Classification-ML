de8acecb56893cd14af09936b15b93609046b11d
Hosein021X <71443585+Hosein021X@users.noreply.github.com>
2026-04-05 22:13:17 +0200

    Update Preprocessing_Script.py

--- src/Preprocessing_Script.py
+++ src/Preprocessing_Script.py
@@ -2,6 +2,9 @@ import string
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 
 def clean_text(text):
+    """
+    Lowercases, removes punctuation/digits, and filters out stop words.
+    """
     text = str(text).lower()
     text = ''.join([c for c in text if c not in string.punctuation])
     text = ''.join([c for c in text if not c.isdigit()])
